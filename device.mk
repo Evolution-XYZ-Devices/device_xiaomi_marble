70e4ab7e982a345c49c318351a5e9afcfe92a581
Pranav Vashi <neobuddy89@gmail.com>
2022-06-25 02:41:51 +0530

    marble: Enable blur
    Change-Id: Ib2d7c7b893d0f12e2c52f47ae68f7ab28dda63e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -115,6 +115,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
