f60f4d62d0b19351157bee4bc0fa28c16d776274
LuK1337 <priv.luk@gmail.com>
2023-06-14 21:27:44 +0900

    marble: Partially import parts from our raphael and sm8350-common trees
    * https://github.com/Evolution-X-Devices/device_xiaomi_raphael/commits/tiramisu/parts/
    * https://github.com/Evolution-X-Devices/device_xiaomi_sm8350-common/commits/tiramisu/parts/
    
    Updates to:
    
    * Dirac
    * Doze
    
    Added:
    
    * Thermals
    * Per-app refresh rate
    * Clear speaker
    
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,7 @@ include device/qcom/sepolicy/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Vendor boot
