5c2ef2ebdb6fb78a9ce67d1abd414ef0784aea75
aswin7469 <aswinas@pixysos.com>
2023-09-25 22:31:37 +0530

    marble: Import SpatialAudio
    Change-Id: a421a3d97bd4694bb746b614460433c24d663d6f
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -557,6 +557,21 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Spatial Audio
+PRODUCT_PACKAGES += \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml \
+    libspatialaudio
+
+# Spatial Audio: optimize spatializer effect
+PRODUCT_PROPERTY_OVERRIDES += \
+       audio.spatializer.effect.util_clamp_min=300
+
+# Spatial Audio: declare use of spatial audio
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.audio.spatializer_enabled=true \
+       ro.audio.spatializer_transaural_enabled_default=false \
+       persist.vendor.audio.spatializer.speaker_enabled=true
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
