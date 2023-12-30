d471a2e2dcd1121cf47417c4f118aa56e1ea1687
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-30 08:19:22 +0530

    marble: Bring in gps.conf from GPS HAL
    https://git.codelinaro.org/clo/la/platform/hardware/qcom/gps/-/blob/LA.VENDOR.1.0.r1-24300-WAIPIO.QSSI13.0/etc/gps.conf

--- device.mk
+++ device.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-marble \
