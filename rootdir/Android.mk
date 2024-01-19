4c54972c214cf3c2228bd3cf4ae0ce1056bc27b5
Joey Huab <joey@evolution-x.org>
2024-01-19 22:39:57 +0900

    marble: Move charger_fw_fstab.qti to vendor tree

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -109,14 +109,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := charger_fw_fstab.qti
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/charger_fw_fstab.qti
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd-odm.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
