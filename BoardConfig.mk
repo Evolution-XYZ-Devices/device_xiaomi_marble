4b526cd604412a1f0dbd49bac6870a003254eb88
Nippon <adamayyad1999@hotmail.com>
2024-02-27 22:40:52 +0530

    marble: BoardConfig: set HWUI_COMPILE_FOR_PERF to "true"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,9 @@ AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 
 TARGET_USES_QCOM_MM_AUDIO := true
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Boot control
 SOONG_CONFIG_NAMESPACES += ufsbsg
 SOONG_CONFIG_ufsbsg += ufsframework
