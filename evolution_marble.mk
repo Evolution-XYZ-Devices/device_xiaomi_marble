ef8538cbe2c82904faffac262e654741ef578be5
Joey <joey@evolution-x.org>
2024-04-05 16:17:56 +0900

    marble: Drop libinit
    * Causing bootloop and tons of headache

--- evolution_marble.mk
+++ evolution_marble.mk
@@ -23,7 +23,18 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 23049PCD8G
 PRODUCT_NAME := evolution_marble
+
+PRODUCT_SYSTEM_NAME := marble_global
+PRODUCT_SYSTEM_DEVICE := marble
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="marble_global-user 12 SKQ1.230401.001 V816.0.3.0.UMRMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := POCO/marble_global/marble:12/SKQ1.230401.001/V816.0.3.0.UMRMIXM:user/release-keys
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
